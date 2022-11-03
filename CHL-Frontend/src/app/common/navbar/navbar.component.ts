fe8ff3e4fc6f62694b0991e1b868af81d852fb93
Kaur Khanuja_ Tashmeet (tkaurkhanuja@deloitte.com) <tkaurkhanuja@your.hostname.com>
2022-11-03 21:01:53 +0530

    navbar and button components

--- CHL-Frontend/src/app/common/navbar/navbar.component.ts
+++ CHL-Frontend/src/app/common/navbar/navbar.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  styles: [`:host ::ng-deep .btn{ height: 30px;padding-bottom: 30px;
+    padding-top: 0px !important }`] 
+  // to change the style of child component
 })
 export class NavbarComponent implements OnInit {
-
+  btntext: string='Login';
+  
   constructor() { }
 
   ngOnInit(): void {
