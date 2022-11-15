0d6e971b2749b1c7368433f139e06627a7a61aad
akanksha5 <akanksha5@deloitte.com>
2022-11-15 17:18:14 +0530

    updated code

--- CHL-Frontend/src/app/components/patient/personal-details/personal-details.component.ts
+++ CHL-Frontend/src/app/components/patient/personal-details/personal-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-personal-details',
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonalDetailsComponent implements OnInit {
 
+
+  @Input('btntext') btntext: string='Login';
   constructor() { }
 
   ngOnInit(): void {
+  }
+  textBtnConfig = {
+    styles: {
+      backgroundColor: '#1CB5BD',
+      color: '#ffff',
+      fontFamily: 'Montserrat',
+      fontSize: '20px',
+      // borderColor: '#1CB5BD',
+      
+      // padding: '8px 54px',
+      gap: '10px',
+      width: '150px',
+      height: '40px',
+      border: '1px solid #1CB5BD',
+      borderRadius: '4px',
+      
+
+    },text:['']
   }
 
 }
