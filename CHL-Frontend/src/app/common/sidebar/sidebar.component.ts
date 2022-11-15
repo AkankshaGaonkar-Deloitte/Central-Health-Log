ac6f062031f0491be17b1b447c9450d668e8c88e
tashmeetkaur-deloitte <tkaurkhanuja@deloitte.com>
2022-11-15 19:48:04 +0530

    updated sidebar

--- CHL-Frontend/src/app/common/sidebar/sidebar.component.ts
+++ CHL-Frontend/src/app/common/sidebar/sidebar.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+menus={'1':["Dashboard","/patient-dashboard"],'2':["Patient Details","/patient-details"],'3':["Past Records","/past-records"]};
+Dashboard:string="Dashboard";
 
   constructor() { }
+  
 
   ngOnInit(): void {
   }
