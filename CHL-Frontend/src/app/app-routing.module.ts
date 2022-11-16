8985efcd943004e18bd4e074b3cd58963359580d
nawaz-deloitte <111974080+nawaz-deloitte@users.noreply.github.com>
2022-11-16 19:10:33 +0530

    front end removed from task doctor registration

--- CHL-Frontend/src/app/app-routing.module.ts
+++ CHL-Frontend/src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DoctorRegistrationComponent } from './components/doctor-registration/doctor-registration.component';
 
 const routes: Routes = [
-  {path:'doctor-registration', component: DoctorRegistrationComponent}
 ];
 
 @NgModule({
