eca232654afbb4ed77616abe1a8d8a991073602b
nawaz-deloitte <111974080+nawaz-deloitte@users.noreply.github.com>
2022-11-16 17:05:46 +0530

    dropdown component updated according to new figma design, also updated the css, now pass only those css which needs to be overrriden

--- CHL-Frontend/src/app/common/dropdown/dropdown.component.ts
+++ CHL-Frontend/src/app/common/dropdown/dropdown.component.ts
@@ -5,6 +5,29 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.scss'],
 })
+
+/*
+//PARENT HTML
+<app-dropdown [states]="optionList" [dropConfig]="textDropConfig"></app-dropdown>
+
+//PARENT TS
+textDropConfig = {
+  //
+    ONLY SEND IN STYLES ONLY IF YOU WANT TO CHANGE THE DEFAULT STYLES
+    styles: {
+      // width: '100%',
+      // paddingTop: '0.5em',
+      // ...
+    }
+  }
+
+  optionList = [
+    "Andaman and Nicobar Islands",
+    "Andhra Pradesh",
+    "Arunachal Pradesh"
+  ];
+*/
+
 export class DropdownComponent implements OnInit {
   @Input() dropConfig!: any;
   @Input() states!: string[];
