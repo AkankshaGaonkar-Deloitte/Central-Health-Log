20b21b210639375c56fdb05116bfc67a4cf4a6a8
akanksha5 <akanksha5@deloitte.com>
2022-11-17 14:02:17 +0530

    updated code

--- CHL-Frontend/src/app/components/patient/medication/medication.component.ts
+++ CHL-Frontend/src/app/components/patient/medication/medication.component.ts
@@ -11,5 +11,23 @@ export class MedicationComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  textBtnConfig = {
+    styles: {
+      backgroundColor: '#1CB5BD',
+      color: '#ffff',
+      fontFamily: 'Montserrat',
+      fontSize: '20px',
+      // borderColor: '#1CB5BD',
+      
+      // padding: '8px 54px',
+      gap: '10px',
+      width: '150px',
+      height: '40px',
+      border: '1px solid #1CB5BD',
+      borderRadius: '4px',
+      
+
+    },text:['']
+  }
 
 }
