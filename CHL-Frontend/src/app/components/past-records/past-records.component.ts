73dbefb9f96a6d4352b18daf52f1169151add10c
nawaz-deloitte <111974080+nawaz-deloitte@users.noreply.github.com>
2022-11-18 00:30:05 +0530

    search bars and filter buttons added to past record section

--- CHL-Frontend/src/app/components/past-records/past-records.component.ts
+++ CHL-Frontend/src/app/components/past-records/past-records.component.ts
@@ -13,20 +13,60 @@ export class PastRecordsComponent implements OnInit {
       background: '#fff',
       color: '#1CB5BD',
       height: '40px',
-      width: '120px',
+      width: '170px',
       fontFamily: 'Montserrat',
       fontStyle: 'normal',
       fontWeight: '500',
       fontSize: '20px',
       lineHeight: '24px',
       textAlign: 'center',
-  
-      }
+
+    }
   };
 
-constructor() { }
+  textBtnConfig2 = {
+    type: "submit",
+    styles: {
+      background: '#1CB5BD',
+      color: '#fff',
+      height: '40px',
+      width: '170px',
+      fontFamily: 'Montserrat',
+      fontStyle: 'normal',
+      fontWeight: '500',
+      fontSize: '20px',
+      lineHeight: '24px',
+      textAlign: 'center',
 
-ngOnInit(): void {
-}
+    }
+  };
+
+  ipConfig = {
+    type: 'search',
+    label: 'from',
+    styling: {
+      width: '197px',
+      height: '40px',
+      background: '#FFFFFF',
+      border: '1px solid #949494',
+      borderRadius: '0'
+    },
+    validations: {
+      required: '',
+      minLength: '',
+      maxLength: '',
+      pattern: ''
+    },
+    patternErrorMessage: 'Invalid ph no'
+  };
+
+  onClickEventReceived($event: string | number | null) {
+    console.log(`At parent ${$event}`);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
