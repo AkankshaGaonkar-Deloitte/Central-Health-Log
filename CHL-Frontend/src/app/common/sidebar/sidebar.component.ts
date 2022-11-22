7b80c810795c3027adfe833598e9d0a99a6a425b
tashmeetkaur-deloitte <tkaurkhanuja@deloitte.com>
2022-11-22 14:14:27 +0530

    changed all common-components css

--- CHL-Frontend/src/app/common/sidebar/sidebar.component.ts
+++ CHL-Frontend/src/app/common/sidebar/sidebar.component.ts
@@ -7,9 +7,24 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-@Input() menus={ '1': ['Dashboard','/patient-dashboard']} ;
-
+//   onClick(tab:any,menus:KeyValue(string:[str])){
+//   console.log(tab[2]);
+//   for (var i of menus) {
+//     console.log(i)
+//     if (i===tab){
+//       i[2]=1;
+//     }
+//     else{
+//       i[2]=0;
+//     }
+// }
+//   tab[2]=1;
+//   console.log(tab[2]);  
+//         }  
+@Input() menus={ '1': ['Dashboard','/patient-dashboard',0]} ;
+tab:number=0;
 Dashboard:string="Dashboard";
+
 
   constructor() { }
 
