733fb6df4ca012380a964ddcbec6e09912cf16e5
tashmeetkaur-deloitte <tkaurkhanuja@deloitte.com>
2022-11-22 17:45:00 +0530

    final common components

--- CHL-Frontend/src/app/common/sidebar/sidebar.component.ts
+++ CHL-Frontend/src/app/common/sidebar/sidebar.component.ts
@@ -7,27 +7,22 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-//   onClick(tab:any,menus:KeyValue(string:[str])){
-//   console.log(tab[2]);
-//   for (var i of menus) {
-//     console.log(i)
-//     if (i===tab){
-//       i[2]=1;
-//     }
-//     else{
-//       i[2]=0;
-//     }
-// }
-//   tab[2]=1;
-//   console.log(tab[2]);  
-//         }  
-@Input() menus={ '1': ['Dashboard','/patient-dashboard',0]} ;
-tab:number=0;
-Dashboard:string="Dashboard";
 
+  onClick(tab: any, menus: any) {
+    for (let key in menus) {
+     if(menus[key]==tab.value){
+        menus[key][2]=1;
+     }else{
+      menus[key][2]=0;
+     }
+
+    }
+  }
+
+  @Input() menus = { '1': ['Dashboard', '/patient-dashboard', 0] };
+  Dashboard: string = "Dashboard";
 
   constructor() { }
-
 
   ngOnInit(): void {
   }
