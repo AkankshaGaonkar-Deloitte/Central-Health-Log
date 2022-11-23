d69a116bd4eb3a31bfb95c873a41a59111573ce7
tashmeetkaur-deloitte <tkaurkhanuja@deloitte.com>
2022-11-23 14:15:24 +0530

    updated

--- CHL-Frontend/src/app/app.component.ts
+++ CHL-Frontend/src/app/app.component.ts
@@ -8,5 +8,4 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'CHL-Frontend';
-  menus={'1':["Dashboard","/patient-dashboard",1],'2':["Patient Details","/patient-details",0],'3':["Past Records","/past-records",0]};
 }
