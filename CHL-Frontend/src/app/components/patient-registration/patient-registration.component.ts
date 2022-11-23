7844645ed59f56559711dfc36c2775dc3399f381
tashmeetkaur-deloitte <tkaurkhanuja@deloitte.com>
2022-11-23 12:30:18 +0530

    added flex in image container

--- CHL-Frontend/src/app/components/patient-registration/patient-registration.component.ts
+++ CHL-Frontend/src/app/components/patient-registration/patient-registration.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { reduce } from 'rxjs';
 import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
