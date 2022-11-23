b6b3bf9f290f9032818c246744012ca094f7bd82
tashmeetkaur-deloitte <tkaurkhanuja@deloitte.com>
2022-11-23 16:46:36 +0530

    updated alignment

--- CHL-Frontend/src/app/components/login-page/login/login.component.ts
+++ CHL-Frontend/src/app/components/login-page/login/login.component.ts
@@ -9,18 +9,20 @@ export class LoginComponent implements OnInit {
   ipConfig = {
     type: 'text',
     label: 'username',
+    placeholder: '',
     styling: {  
-      width: '200px',
-      height: '40px'
+      width: '15em',
+      height:'2.4em'
     },
     validations : {
       required: '',
-      minLength: '3',
+      minLength: '2',
       maxLength: '',
       pattern: ''
     },
-    patternErrorMessage: ''
-  };
+    patternErrorMessage: 'Must be a valid input'
+  }
+
 
   onClickEventReceived($event: string|number|null) {
     console.log(`At parent ${$event}`);
@@ -58,16 +60,21 @@ export class LoginComponent implements OnInit {
   textBtnConfig = {
     type:'submit',
     styles: {
-      backgroundColor: '#ffff',
-      color: 'black',
+      backgroundColor: '#1CB5BD',
+      color: '#ffff',
       fontFamily: 'Montserrat',
       fontSize: '18px',
       borderColor: '#fff',
       width: '130px',
+      height: '40px',}
+  }
+  textBtnConfig2 = {
+    type:'submit',
+    styles: {
+      fontSize: '18px',
+      width: '130px',
       height: '40px',
-      border: '1px solid #949494',
-      borderRadius: '0px',
-      },text:['']
   }
 
+}
 }
