2324329b1a61aa29656c867c5c0c593e76f6433b
akanksha5 <akanksha5@deloitte.com>
2022-11-24 23:34:53 +0530

    updated code

---
+++ CHL-Frontend/src/app/components/admin/admin-in-progress/admin-in-progress.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-in-progress',
+  templateUrl: './admin-in-progress.component.html',
+  styleUrls: ['./admin-in-progress.component.scss']
+})
+export class AdminInProgressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
