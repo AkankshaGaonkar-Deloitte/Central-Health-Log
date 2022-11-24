bf5bda5be62861bbb7e0e64af0c40ed1a4c190dd
akanksha5 <akanksha5@deloitte.com>
2022-11-24 23:30:10 +0530

    updated code

--- CHL-Frontend/src/app/app-routing.module.ts
+++ CHL-Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisteredDoctorsComponent } from './components/admin/registered-doctors/registered-doctors.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:RegisteredDoctorsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ CHL-Frontend/src/app/components/admin/registered-doctors/registered-doctors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registered-doctors',
+  templateUrl: './registered-doctors.component.html',
+  styleUrls: ['./registered-doctors.component.scss']
+})
+export class RegisteredDoctorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
