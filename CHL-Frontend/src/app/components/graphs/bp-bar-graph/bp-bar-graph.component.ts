fb36be1191709ba51f24c207a5f0e2441068670f
tashmeetkaur-deloitte <tkaurkhanuja@deloitte.com>
2022-11-28 19:08:53 +0530

    bp graph done

--- CHL-Frontend/src/app/components/graphs/bp-bar-graph/bp-bar-graph.component.ts
+++ CHL-Frontend/src/app/components/graphs/bp-bar-graph/bp-bar-graph.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import { ILoadedEventArgs, ChartTheme } from '@syncfusion/ej2-angular-charts';
+import { GraphServiceService } from 'src/app/service/graph-service.service';
 
 @Component({
-  selector: 'app-bp-bar-graph',
+  selector: 'bp-graph',
   templateUrl: './bp-bar-graph.component.html',
   styleUrls: ['./bp-bar-graph.component.scss']
 })
 export class BpBarGraphComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private graphService: GraphServiceService) { }
+  public BP: Object[]=[] ;
+  public primaryXAxis: Object = {
+    title: 'Date',
+    valueType: 'Category',
+    edgeLabelPlacement: 'Shift',
+    majorGridLines: { width: 0 }
+  };
+  //Initializing Primary Y Axis
+  public primaryYAxis: Object = {
+    title: 'BP',
+    minimum: 50,
+    maximum: 190,
+    interval: 10,
+    lineStyle: { width: 0},
+    majorTickLines: { width: 0 },
+  };
+  public chartArea: Object = {
+    border: {
+      width: 0
+    }
+  };
+  // public width: string = Browser.isDevice ? '100%' : '75%';
+  public tooltip: Object = {
+    enable: true
+  };
+  public legend: Object = {
+    visible: true,
+    enableHighlight: true
+  }
+  // custom code start
+  public load(args: ILoadedEventArgs): void {
+    let selectedTheme: string = location.hash.split('/')[1];
+    selectedTheme = selectedTheme ? selectedTheme : 'Material';
+    args.chart.theme = <ChartTheme>(selectedTheme.charAt(0).toUpperCase() + selectedTheme.slice(1)).replace(/-dark/i, "Dark");
+  };
+  // custom code end
+  public title: string = 'Blood Pressure';
   ngOnInit(): void {
+    let pr:Object[]=[];
+    this.graphService.getBloodPressure().subscribe(data =>{
+    for(let i in data) {
+      pr.push(data[i]);
+      console.warn(data[i]);
+    }
+    this.BP=pr;
+  }
+    
+    )
   }
 
 }
