303e61acf725c27008ba359821855b9bb10b3411
tashmeetkaur-deloitte <tkaurkhanuja@deloitte.com>
2022-11-28 18:22:36 +0530

    graphs not working together

--- CHL-Frontend/src/app/service/graph-service.service.ts
+++ CHL-Frontend/src/app/service/graph-service.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { BloodPressure } from '../model/BloodPressure';
+import { BMI } from '../model/BMI';
 import { Pulse } from '../model/Pulse';
 
 
@@ -15,13 +17,13 @@ export class GraphServiceService {
       return this.httpClient.get<Pulse[]>(`${this.baseurl+'/pulse'}`)
 
     }
-    // getBMI():Observable<BMI[]>{
-    //   return this.httpClient.get<BMI[]>(`${this.baseurl+'/bmi'}`)
+    getBMI():Observable<BMI[]>{
+      return this.httpClient.get<BMI[]>(`${this.baseurl+'/bmi'}`)
 
-    // }
-    // getBloodPressure():Observable<BloodPressure[]>{
-    //   return this.httpClient.get<BloodPressure[]>(`${this.baseurl+'/bp'}`)
+    }
+    getBloodPressure():Observable<BloodPressure[]>{
+      return this.httpClient.get<BloodPressure[]>(`${this.baseurl+'/bp'}`)
 
-    // }
+    }
   }
 
