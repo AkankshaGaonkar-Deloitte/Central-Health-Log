42f11542120215e3f5ce983ab41ef44666fcc87f
nawaz-deloitte <111974080+nawaz-deloitte@users.noreply.github.com>
2022-11-29 08:22:23 +0530

    routing module added

--- CHL-Frontend/src/app/app-routing.module.ts
+++ CHL-Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PastRecordsComponent } from './components/past-records/past-records.component';
 
 const routes: Routes = [
+  {path:'past-records', component: PastRecordsComponent}
 ];
 
 @NgModule({
