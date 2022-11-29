d3ee120d26c3afdaa45abf9f098f597a8ede0602
tashmeetkaur-deloitte <tkaurkhanuja@deloitte.com>
2022-11-29 12:52:11 +0530

    added headers in service

--- CHL-Frontend/src/app/service/graph-service.service.ts
+++ CHL-Frontend/src/app/service/graph-service.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BloodPressure } from '../model/BloodPressure';
@@ -13,17 +13,35 @@ export class GraphServiceService {
   private baseurl="http://localhost:8080/graph";
 
   constructor(private httpClient:HttpClient) { }
+  createAuthenticationHeader(){
+    let username: string = 'nawaz2000'
+    let password: string = '123'
+    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+    //return basicAuthHeaderString;
+    return new HttpHeaders({
+      Authorization: basicAuthHeaderString
+      })
+      }
     getPulse():Observable<Pulse[]>{
-      return this.httpClient.get<Pulse[]>(`${this.baseurl+'/pulse'}`)
+      let headers = this.createAuthenticationHeader()
+      return this.httpClient.get<Pulse[]>(`${this.baseurl+'/pulse'}`, {headers})
 
     }
     getBMI():Observable<BMI[]>{
-      return this.httpClient.get<BMI[]>(`${this.baseurl+'/bmi'}`)
+      let headers = this.createAuthenticationHeader()
+      return this.httpClient.get<BMI[]>(`${this.baseurl+'/bmi'}`, {headers})
 
     }
     getBloodPressure():Observable<BloodPressure[]>{
-      return this.httpClient.get<BloodPressure[]>(`${this.baseurl+'/bp'}`)
+      let headers = this.createAuthenticationHeader()
+      return this.httpClient.get<BloodPressure[]>(`${this.baseurl+'/bp'}`, {headers})
 
     }
+    
+      
+      // getAllPatientRecords(patientId: number): Observable<PastRecord[]>{
+      // let headers = this.createAuthenticationHeader()
+      // return this.http.get<PastRecord[]>(`http://localhost:8080/patient/past-record/${patientId}`, {headers})
+      // }
   }
 
