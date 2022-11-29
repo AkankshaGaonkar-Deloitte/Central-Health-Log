82216b0180c767c6c709d3fae6840e8fc66a35cf
tashmeetkaur-deloitte <tkaurkhanuja@deloitte.com>
2022-11-29 14:27:28 +0530

    added reg services

---
+++ CHL-Frontend/src/app/model/doctor.ts
@@ -0,0 +1,18 @@
+export class Doctor {
+    id !: number;
+    aadharNo !: string|number;
+    username!: string;
+    password!:string;
+    phoneNo !: number;
+    email !: string;
+    doctorname !: string;
+    qualification !: string;
+    speciality !: string;
+    regNo !: string|number;
+    yearofReg !: number;
+    stateMedCouncil !: string;
+    age !: number;
+    gender !: string;
+    address !:string;
+    statusCode!:string;
+}
--- CHL-Frontend/src/app/service/patient/patient-reg.service.ts
+++ CHL-Frontend/src/app/service/patient/patient-reg.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Doctor } from 'src/app/model/doctor';
 import { Patient } from 'src/app/model/patient';
 
 @Injectable({
@@ -18,17 +19,18 @@ export class PatientRegService {
       Authorization: basicAuthHeaderString
       })
       }
-      baseurl= 'http://localhost:8080';
+
+      private baseurl="http://localhost:8080";
     addPatient(newPatient:Patient):Observable<Patient[]>{
       let headers = this.createAuthenticationHeader()
       return this.httpClient.post<Patient[]>(`${this.baseurl+'/patient'}`, newPatient,{headers})
 
     }
-    // addDoctor():Observable<Doctor[]>{
-    //   let headers = this.createAuthenticationHeader()
-    //   return this.httpClient.post<Doctor[]>(`${this.baseurl+'/doctor'}`, {headers})
+    addDoctor(newDoctor:Doctor):Observable<Doctor[]>{
+      let headers = this.createAuthenticationHeader()
+      return this.httpClient.post<Doctor[]>(`${this.baseurl+'/doctor'}`,newDoctor, {headers})
 
-    // }
+    }
    
     
 }
