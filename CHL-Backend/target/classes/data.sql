1ee377c8015cc6b0f181b37f2eb83088afd9f5a9
nawaz-deloitte <111974080+nawaz-deloitte@users.noreply.github.com>
2022-11-29 08:19:20 +0530

    uploaded by added on past record table view

---
+++ CHL-Backend/target/classes/data.sql
@@ -0,0 +1,10 @@
+INSERT
+INTO
+  past_record
+  (patient_id, doctor_id, doctor_name, upload_date, diagnosis, uploaded_by, severity)
+VALUES
+  (43190, 45241, 'Dr. Prasad', '2022-11-21', 'Polio', 'Doctor', 9),
+  (43190, 45241, 'Dr. Prasad', '2012-9-12', 'Tuberculosis', 'Doctor', 6),
+  (43190, 33421, 'Dr. Tashmeet', '2021-01-25', 'Hep-C', 'Doctor', 8),
+  (43190, 55765, 'Dr. Akanksha', '2019-10-15', 'Pneumonia', 'Self', 5),
+  (89197, 33421, 'Dr. Tashmeet', '2011-09-17', 'Hep-A', 'Doctor', 4);
