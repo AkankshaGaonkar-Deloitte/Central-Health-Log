2329c533e5380bd190197dce004cbb5522599697
tashmeetkaur-deloitte <tkaurkhanuja@deloitte.com>
2022-11-29 14:10:18 +0530

    added services

--- CHL-Frontend/src/app/service/patient/patient-reg.service.ts
+++ CHL-Frontend/src/app/service/patient/patient-reg.service.ts
@@ -1,10 +1,31 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatientRegService {
 
-  constructor(httpClient:HttpClient) { }
+  constructor(private httpClient:HttpClient) { }
+  createAuthenticationHeader(){
+    let username: string = 'nawaz2000'
+    let password: string = '123'
+    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+    //return basicAuthHeaderString;
+    return new HttpHeaders({
+      Authorization: basicAuthHeaderString
+      })
+      }
+      // baseurl=
+    // addPatient():Observable<Patient[]>{
+    //   let headers = this.createAuthenticationHeader()
+    //   return this.httpClient.post<Patient[]>(`${this.baseurl+'/patient'}`, {headers})
+
+    // }
+    // addDoctor():Observable<Doctor[]>{
+    //   let headers = this.createAuthenticationHeader()
+    //   return this.httpClient.post<Doctor[]>(`${this.baseurl+'/doctor'}`, {headers})
+
+    // }
 }
