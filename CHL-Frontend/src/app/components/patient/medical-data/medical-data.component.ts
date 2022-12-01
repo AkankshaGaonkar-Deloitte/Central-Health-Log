185a2009d35f180d47bb4d26065274883adffb51
nawaz-deloitte <111974080+nawaz-deloitte@users.noreply.github.com>
2022-12-01 15:12:31 +0530

    redesigning the medical data page. Outline created

--- CHL-Frontend/src/app/components/patient/medical-data/medical-data.component.ts
+++ CHL-Frontend/src/app/components/patient/medical-data/medical-data.component.ts
@@ -1,7 +1,7 @@
-import { Vitals } from './../../../model/patient/vitals';
-import { PatientService } from './../../../service/patient/patient.service';
 import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { PastRecord } from 'src/app/model/past-record';
+import { Medication } from 'src/app/model/patient/medication';
+import { MedicationService } from 'src/app/service/medication/medication.service';
 
 @Component({
   selector: 'app-medical-data',
@@ -9,225 +9,201 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./medical-data.component.scss'],
 })
 export class MedicalDataComponent implements OnInit {
-  menus = {
-    '1': ['Dashboard', '/patient-dashboard', 0],
-    '2': ['Personal Details', '/personal-details', 0],
-    '3': ['Medical Data', '/medical-data', 1],
-    '4': ['Medications', '/medications', 0],
-    '5': ['Past Records', '/past-records', 0],
-  };
-  constructor(private medicalDataService: PatientService) {}
-  
-  vitals=new Vitals();
-  ngOnInit(): void {
-    this.getVitalById();
-  }
-  MedicationComponent = new FormGroup({
-    height: new FormControl(''),
-    bmi: new FormControl(''),
-    immunization: new FormControl(''),
-    familyMedicalHistory: new FormControl(''),
-    weight: new FormControl(''),
-    allergies: new FormControl(''),
-    bloodGroup: new FormControl(''),
-    chronicHealthProblem: new FormControl(''),
-  });
+  pastRecord: PastRecord = new PastRecord(-1)
+  medicationData: Medication = new Medication(-1)
 
-  heightReceived($event: any) {
-    this.MedicationComponent.patchValue({ height: $event });
-    this.vitals.height = Number(this.MedicationComponent.value.height);
-  }
-  bmiReceived($event: any) {
-    this.MedicationComponent.patchValue({ bmi: $event });
-    this.vitals.bmi = Number(this.MedicationComponent.value.bmi);
-  }
-  immunizationsReceived($event: any) {
-    this.MedicationComponent.patchValue({ immunization: $event });
-    this.vitals.immunizations = String(this.MedicationComponent.value.immunization);
-  }
-  familymedicalhistoryReceived($event: any) {
-    this.MedicationComponent.patchValue({ familyMedicalHistory: $event });
-    this.vitals.familyMedicalHistory = String(this.MedicationComponent.value.familyMedicalHistory);
-  }
-  weightReceived($event: any) {
-    this.MedicationComponent.patchValue({ weight: $event });
-    this.vitals.weight = Number(this.MedicationComponent.value.weight);
-  }
-  allergiesReceived($event: any) {
-    this.MedicationComponent.patchValue({ allergies: $event });
-    this.vitals.allergies = String(this.MedicationComponent.value.allergies);
-  }
+  patientId: number = 43190
 
-  bloodGroupReceived($event: any) {
-    this.MedicationComponent.patchValue({ bloodGroup: $event });
-    this.vitals.bloodGroup = String(this.MedicationComponent.value.bloodGroup);
-  }
+  filterSelector: string = 'Date'
+  dateFrom: string = ''
+  dateTo: string = ''
+  severityFrom: number = -1
+  severityTo: number = -1
+  doctor: number = -1
 
-  chronichealthproblemReceived($event: any) {
-    this.MedicationComponent.patchValue({ chronicHealthProblem: $event });
-    this.vitals.chronicHealthProblems = String(this.MedicationComponent.value.chronicHealthProblem);
-    console.log(this.vitals.chronicHealthProblems+" prasad");
-  }
+  allMedicalDataOfAPatient: Medication[] = []
 
-  ipConfig1 = {
-    type: 'text',
-    label: 'height',
-    placeholder: '',
-    styling: {
-      width: '300px',
-      height: '40px',
-    },
-    validations: {
-      required: '',
-      minLength: '',
-      maxLength: '',
-      pattern: '',
-    },
-    patternErrorMessage: '',
-  };
-  ipConfig2 = {
-    type: 'text',
-    label: 'weight',
-    placeholder: '',
-    styling: {
-      width: '300px',
-      height: '40px',
-    },
-    validations: {
-      required: '',
-      minLength: '',
-      maxLength: '',
-      pattern: '',
-    },
-    patternErrorMessage: '',
-  };
-  ipConfig3 = {
-    type: 'text',
-    label: 'bmi',
-    placeholder: '',
-    styling: {
-      width: '300px',
-      height: '40px',
-    },
-    validations: {
-      required: '',
-      minLength: '',
-      maxLength: '',
-      pattern: '',
-    },
-    patternErrorMessage: '',
-  };
-  ipConfig4 = {
-    type: 'text',
-    label: 'allergies',
-    placeholder: '',
-    styling: {
-      width: '300px',
-      height: '40px',
-    },
-    validations: {
-      required: '',
-      minLength: '',
-      maxLength: '',
-      pattern: '',
-    },
-    patternErrorMessage: '',
-  };
-  ipConfig5 = {
-    type: 'text',
-    label: 'immunizations',
-    placeholder: '',
-    styling: {
-      width: '300px',
-      height: '40px',
-    },
-    validations: {
-      required: '',
-      minLength: '',
-      maxLength: '',
-      pattern: '',
-    },
-    patternErrorMessage: '',
-  };
-  ipConfig6 = {
-    type: 'text',
-    label: 'blood group',
-    placeholder: '',
-    styling: {
-      width: '300px',
-      height: '40px',
-    },
-    validations: {
-      required: '',
-      minLength: '',
-      maxLength: '',
-      pattern: '',
-    },
-    patternErrorMessage: '',
-  };
-  ipConfig7 = {
-    type: 'text',
-    label: 'family medical history',
-    placeholder: '',
-    styling: {
-      width: '300px',
-      height: '40px',
-    },
-    validations: {
-      required: '',
-      minLength: '',
-      maxLength: '',
-      pattern: '',
-    },
-    patternErrorMessage: '',
-  };
-  ipConfig8 = {
-    type: 'text',
-    label: 'chronic health problem',
-    placeholder: '',
-    styling: {
-      width: '300px',
-      height: '40px',
-    },
-    validations: {
-      required: '',
-      minLength: '',
-      maxLength: '',
-      pattern: '',
-    },
-    patternErrorMessage: '',
-  };
   textBtnConfig = {
+    type: "submit",
     styles: {
-      backgroundColor: '#1CB5BD',
-      color: '#ffff',
+      background: '#fff',
+      color: '#1CB5BD',
+      height: '2em',
+      width: '8em',
       fontFamily: 'Montserrat',
-      fontSize: '20px',
-      width: '150px',
-      height: '40px',
-      border: '1px solid #1CB5BD',
-      borderRadius: '4px',
-    },
-    type: 'submit',
+      fontStyle: 'normal',
+      fontWeight: '500',
+      fontSize: '1.25em',
+      lineHeight: '1.5em',
+      textAlign: 'center',
+
+    }
   };
 
-  onupdate() {
-    
-    console.log(this.vitals.chronicHealthProblems +"hi nawaz");
-    return this.medicalDataService.updateVitals(this.vitals).subscribe(data => {
-      this.vitals = data;
-      console.log(this.vitals);
-      
+  textBtnConfig2 = {
+    type: "submit",
+    styles: {
+      background: '#1CB5BD',
+      color: '#fff',
+      height: '2em',
+      width: '8em',
+      fontFamily: 'Montserrat',
+      fontStyle: 'normal',
+      fontWeight: '500',
+      fontSize: '1.25em',
+      lineHeight: '1.5em',
+      textAlign: 'center',
 
-      
-    });
+    }
+  };
+
+  deleteBtnConfig = {
+    type: "submit",
+    styles: {
+      background: '#fff',
+      color: '#737373',
+      border: '1px solid #737373',
+      height: '2em',
+      width: '8em',
+      fontFamily: 'Montserrat',
+      fontStyle: 'normal',
+      fontWeight: '500',
+      fontSize: '1.25em',
+      lineHeight: '1.5em',
+      textAlign: 'center',
+
+    }
+  };
+
+  dateConfig = {
+    type: 'date',
+    label: 'Date',
+    placeholder: '',
+    styling: {
+      width: '12.5em',
+      height: '2.5em'
+    },
+    validations: {
+      required: 'false',
+      minLength: '',
+      maxLength: '',
+      pattern: ''
+    },
+    patternErrorMessage: ''
+  };
+
+  idConfig = {
+    type: 'number',
+    label: 'ID',
+    placeholder: '',
+    styling: {
+      width: '12.5em',
+      height: '2.5em'
+    },
+    validations: {
+      required: '',
+      minLength: '',
+      maxLength: '',
+      pattern: ''
+    },
+    patternErrorMessage: ''
+  };
+
+  nameConfig = {
+    type: 'text',
+    label: 'Name',
+    placeholder: '',
+    styling: {
+      width: '12.5em',
+      height: '2.5em'
+    },
+    validations: {
+      required: '',
+      minLength: '4',
+      maxLength: '',
+      pattern: ''
+    },
+    patternErrorMessage: ''
+  };
+
+  diagnosisConfig = {
+    type: 'text',
+    label: 'Diagnosis',
+    placeholder: '',
+    styling: {
+      width: '12.5em',
+      height: '2.5em'
+    },
+    validations: {
+      required: '',
+      minLength: '4',
+      maxLength: '',
+      pattern: ''
+    },
+    patternErrorMessage: ''
+  };
+
+  presscriptionConfig = {
+    type: 'file',
+    label: 'Description',
+    placeholder: '',
+    styling: {
+      width: '12.5em',
+      height: '2.5em'
+    },
+    validations: {
+      required: 'false',
+      minLength: '',
+      maxLength: '',
+      pattern: ''
+    },
+    patternErrorMessage: ''
+  };
+
+
+  menus = { '1': ["Dashboard", "/patient-dashboard", 0], '2': ["Personal Details", "/personal-details", 0], '3': ["Medical Data", "/medical-data", 0], '4': ["Medications", "/medications", 1], '5': ["Past Records", "/past-records", 0] };
+
+  constructor(private medicationService: MedicationService) { }
+
+  ngOnInit(): void {
+    this.medicationService.getAllPatientRecords(this.patientId)
+      .subscribe(response => {
+        this.allMedicalDataOfAPatient = response
+        console.log(this.allMedicalDataOfAPatient);
+        
+      }
+    )
+
   }
-  getVitalById() {
-    return this.medicalDataService.getVitalsById(1).subscribe(data => {
-     this.vitals = data;
-    console.log("db value : " + this.vitals.allergies);
-    
-   });
+
+  removeOrDeleteMedication(id?: number){
+    this.medicationService.removeOrDeleteMedication(id)
+      .subscribe(
+        response => {
+          this.medicationService.getAllPatientRecords(this.patientId)
+            .subscribe(data => this.allMedicalDataOfAPatient = data)
+        }
+      )
+  }
+
+  displayStyle = "none";
+
+  openPopup() {
+    this.displayStyle = "block";
+  }
+  closePopup() {
+    this.displayStyle = "none";
+  }
+
+  submitNewMedication(){
+    this.medicationData.isCurrent=true
+    this.medicationData.patientId=this.patientId
+    this.medicationService.addMedication(this.medicationData)
+      .subscribe(data => {
+        console.log(`Newly added data ${data}`);
+        this.allMedicalDataOfAPatient.push(data)
+      })
+    this.closePopup()
   }
 
   
