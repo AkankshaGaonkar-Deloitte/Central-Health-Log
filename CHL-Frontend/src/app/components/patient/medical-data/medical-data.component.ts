79c059700e4795052a4727f45973bac7054757e6
nawaz-deloitte <111974080+nawaz-deloitte@users.noreply.github.com>
2022-12-01 16:40:13 +0530

    text area added and redesigned in update details form

--- CHL-Frontend/src/app/components/patient/medical-data/medical-data.component.ts
+++ CHL-Frontend/src/app/components/patient/medical-data/medical-data.component.ts
@@ -1,6 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { PastRecord } from 'src/app/model/past-record';
-import { Medication } from 'src/app/model/patient/medication';
 import { MedicationService } from 'src/app/service/medication/medication.service';
 
 @Component({
@@ -9,38 +7,10 @@ import { MedicationService } from 'src/app/service/medication/medication.service
   styleUrls: ['./medical-data.component.scss'],
 })
 export class MedicalDataComponent implements OnInit {
-  pastRecord: PastRecord = new PastRecord(-1)
-  medicationData: Medication = new Medication(-1)
 
-  patientId: number = 43190
-
-  filterSelector: string = 'Date'
-  dateFrom: string = ''
-  dateTo: string = ''
-  severityFrom: number = -1
-  severityTo: number = -1
-  doctor: number = -1
-
-  allMedicalDataOfAPatient: Medication[] = []
+  heightPrefill = ''
 
   textBtnConfig = {
-    type: "submit",
-    styles: {
-      background: '#fff',
-      color: '#1CB5BD',
-      height: '2em',
-      width: '8em',
-      fontFamily: 'Montserrat',
-      fontStyle: 'normal',
-      fontWeight: '500',
-      fontSize: '1.25em',
-      lineHeight: '1.5em',
-      textAlign: 'center',
-
-    }
-  };
-
-  textBtnConfig2 = {
     type: "submit",
     styles: {
       background: '#1CB5BD',
@@ -57,103 +27,68 @@ export class MedicalDataComponent implements OnInit {
     }
   };
 
-  deleteBtnConfig = {
-    type: "submit",
-    styles: {
-      background: '#fff',
-      color: '#737373',
-      border: '1px solid #737373',
-      height: '2em',
-      width: '8em',
-      fontFamily: 'Montserrat',
-      fontStyle: 'normal',
-      fontWeight: '500',
-      fontSize: '1.25em',
-      lineHeight: '1.5em',
-      textAlign: 'center',
-
-    }
-  };
-
-  dateConfig = {
-    type: 'date',
-    label: 'Date',
-    placeholder: '',
-    styling: {
-      width: '12.5em',
-      height: '2.5em'
-    },
-    validations: {
-      required: 'false',
-      minLength: '',
-      maxLength: '',
-      pattern: ''
-    },
-    patternErrorMessage: ''
-  };
-
-  idConfig = {
+  heightConfig = {
     type: 'number',
-    label: 'ID',
+    label: 'Height',
     placeholder: '',
     styling: {
-      width: '12.5em',
-      height: '2.5em'
+      height: '2.375em',
+      width: '100%'
     },
     validations: {
       required: '',
-      minLength: '',
-      maxLength: '',
+      minLength: '1',
+      maxLength: '2',
       pattern: ''
     },
     patternErrorMessage: ''
   };
 
-  nameConfig = {
+  weightConfig = {
+    type: 'number',
+    label: 'Weight',
+    placeholder: '',
+    styling: {
+      height: '2.375em',
+      width: '100%'
+    },
+    validations: {
+      required: '',
+      minLength: '2',
+      maxLength: '3',
+      pattern: ''
+    },
+    patternErrorMessage: ''
+  };
+
+  bmiConfig = {
+    type: 'number',
+    label: 'BMI',
+    placeholder: '',
+    styling: {
+      height: '2.375em',
+      width: '100%'
+    },
+    validations: {
+      required: '',
+      minLength: '2',
+      maxLength: '4',
+      pattern: ''
+    },
+    patternErrorMessage: ''
+  };
+
+  allergiesConfig = {
     type: 'text',
-    label: 'Name',
+    label: 'Allergies',
     placeholder: '',
     styling: {
-      width: '12.5em',
-      height: '2.5em'
+      height: '2.375em',
+      width: '100%'
     },
     validations: {
       required: '',
-      minLength: '4',
-      maxLength: '',
-      pattern: ''
-    },
-    patternErrorMessage: ''
-  };
-
-  diagnosisConfig = {
-    type: 'text',
-    label: 'Diagnosis',
-    placeholder: '',
-    styling: {
-      width: '12.5em',
-      height: '2.5em'
-    },
-    validations: {
-      required: '',
-      minLength: '4',
-      maxLength: '',
-      pattern: ''
-    },
-    patternErrorMessage: ''
-  };
-
-  presscriptionConfig = {
-    type: 'file',
-    label: 'Description',
-    placeholder: '',
-    styling: {
-      width: '12.5em',
-      height: '2.5em'
-    },
-    validations: {
-      required: 'false',
-      minLength: '',
+      minLength: '3',
       maxLength: '',
       pattern: ''
     },
@@ -161,50 +96,11 @@ export class MedicalDataComponent implements OnInit {
   };
 
 
-  menus = { '1': ["Dashboard", "/patient-dashboard", 0], '2': ["Personal Details", "/personal-details", 0], '3': ["Medical Data", "/medical-data", 0], '4': ["Medications", "/medications", 1], '5': ["Past Records", "/past-records", 0] };
+  menus = { '1': ["Dashboard", "/patient-dashboard", 0], '2': ["Personal Details", "/personal-details", 0], '3': ["Medical Data", "/medical-data", 1], '4': ["Medications", "/medications", 0], '5': ["Past Records", "/past-records", 0] };
 
   constructor(private medicationService: MedicationService) { }
 
-  ngOnInit(): void {
-    this.medicationService.getAllPatientRecords(this.patientId)
-      .subscribe(response => {
-        this.allMedicalDataOfAPatient = response
-        console.log(this.allMedicalDataOfAPatient);
-        
-      }
-    )
-
-  }
-
-  removeOrDeleteMedication(id?: number){
-    this.medicationService.removeOrDeleteMedication(id)
-      .subscribe(
-        response => {
-          this.medicationService.getAllPatientRecords(this.patientId)
-            .subscribe(data => this.allMedicalDataOfAPatient = data)
-        }
-      )
-  }
-
-  displayStyle = "none";
-
-  openPopup() {
-    this.displayStyle = "block";
-  }
-  closePopup() {
-    this.displayStyle = "none";
-  }
-
-  submitNewMedication(){
-    this.medicationData.isCurrent=true
-    this.medicationData.patientId=this.patientId
-    this.medicationService.addMedication(this.medicationData)
-      .subscribe(data => {
-        console.log(`Newly added data ${data}`);
-        this.allMedicalDataOfAPatient.push(data)
-      })
-    this.closePopup()
-  }
+  ngOnInit(): void {}
 
   
 }
