2e590134f162a8d667279680a1bc9ff011588962
tashmeetkaur-deloitte <tkaurkhanuja@deloitte.com>
2022-12-03 23:23:53 +0530

    updated backend

---
+++ CHL-Backend/src/main/java/com/CentralHealthLog/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,14 @@
+package com.CentralHealthLog.exception;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+
+    private String message;
+
+    public UsernameAlreadyExistsException() {}
+
+    public UsernameAlreadyExistsException(String msg)
+    {
+        super(msg);
+        this.message = msg;
+    }
+}
