ca6e9260b0f85f7bb59cb40ebc4df26c3c4e1d07
tashmeetkaur-deloitte <tkaurkhanuja@deloitte.com>
2022-12-03 21:43:44 +0530

    added dropdown in reg

--- CHL-Frontend/src/app/components/patient/patient-registration/patient-registration.component.ts
+++ CHL-Frontend/src/app/components/patient/patient-registration/patient-registration.component.ts
@@ -18,11 +18,47 @@ export class PatientRegistrationComponent implements OnInit {
   patient = new Patient();
 
   doctor = new Doctor();
-  textBtnConfig = { type: "submit", styles: { backgroundColor: '#1CB5BD', color: '#fff', height: '2em',width:'8em',fontSize:'1.1em' } };
+  textBtnConfig = { type: "submit", styles: { backgroundColor: '#1CB5BD', 
+  color: '#fff', height: '2em',width:'8em',fontSize:'1.1em' } };
 
   // onUSerID(){
   //   this.ipConfig.patternErrorMessage
   // }
+  optionList = [
+    "Female",
+    "Male",
+    "Other"
+  ];
+  optionList1 = [
+    "Andhra Pradesh Medical Council	",
+	"Arunachal Pradesh Medical Council",	
+	"Assam Medical Council",
+	"Bihar Medical Council"	,
+	"Chattisgarh Medical Council"	,	"Delhi Medical Council"	,
+	"Goa Medical Council",
+	"Gujarat Medical Council",	
+	"Haryana State Dental & Medical Council",	
+"Himachal Pradesh Medical Council"	,
+	"Jammu & Kashmir Medical Council",
+	"Jharkhand Medical Council",
+	"Karnataka Medical Council",
+	"Kerala Medical Council",
+	"Madhya Pradesh Medical Council",
+	"Maharashtra Medical Council",
+	"Manipur Medical Council",
+	"Meghalya Medical Council",		"Mizoram Medical Council"	,"Nagaland Medical Council",	
+	"Orissa Medical Council"	,
+	"Punjab Medical Council",
+	"Rajasthan Medical Council",	
+	"Sikkim Medical Council"	,
+	"Tamil Nadu Medical Council",	
+	"Telangana Medical Council"	,
+	"Tripura Medical Council",
+	"Uttarnchal Medical Council",	
+	"Uttar Pradesh Medical Council",	
+	"West Bengal Medical Council"
+    
+  ];
   ipConfig = {
     type: 'text',
     label: 'firstname',
@@ -81,7 +117,7 @@ export class PatientRegistrationComponent implements OnInit {
     styling: {
       width: '17em',
       height: '2em',
-      borderRadius: '0.2em'
+      borderRadius: '0.2em',
     },
     validations: {
       required: '',
@@ -94,6 +130,14 @@ export class PatientRegistrationComponent implements OnInit {
 
   constructor(private router: Router, private patientregService: PatientRegService) { }
 
+  textDropConfig = {
+    styles: {
+      height: '2em',
+      width: '17em',
+      borderRadius: '0.2em',
+      border: '0.025em solid #949494'
+    }
+  }
   RegistrationForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
