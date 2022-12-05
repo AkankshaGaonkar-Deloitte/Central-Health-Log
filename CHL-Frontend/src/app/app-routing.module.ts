bde639ff7eafd14d3aefce8d1832c429e858644d
akanksha5 <akanksha5@deloitte.com>
2022-12-05 18:50:25 +0530

    updated code

--- CHL-Frontend/src/app/app-routing.module.ts
+++ CHL-Frontend/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DocPatientPastRecordsComponent } from './components/doctor/doc-patient-past-records/doc-patient-past-records.component';
 
-const routes: Routes = [
-];
+const routes: Routes = [{path:'doc-patient-pastrecords',component:DocPatientPastRecordsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
