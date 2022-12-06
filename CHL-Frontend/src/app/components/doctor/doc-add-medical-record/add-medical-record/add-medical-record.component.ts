f85faaf3014f89e9128b25ec6137592e485ee2d7
nawaz-deloitte <111974080+nawaz-deloitte@users.noreply.github.com>
2022-12-06 17:13:57 +0530

    doctor/add-medical-record page designed

--- CHL-Frontend/src/app/components/doctor/doc-add-medical-record/add-medical-record/add-medical-record.component.ts
+++ CHL-Frontend/src/app/components/doctor/doc-add-medical-record/add-medical-record/add-medical-record.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Medication } from 'src/app/model/patient/medication';
+import { MedicationService } from 'src/app/service/medication/medication.service';
 
 @Component({
   selector: 'app-add-medical-record',
@@ -7,14 +9,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddMedicalRecordComponent implements OnInit {
 
-  constructor() { }
+  patientId: number = 43190
+  allMedicalDataOfAPatient: Medication[] = []
 
-  ngOnInit(): void {
-  }
+  textBtnConfig2 = {
+    type: "submit",
+    styles: {
+      background: '#1CB5BD',
+      color: '#fff',
+      height: '2em',
+      width: '8em',
+      fontFamily: 'Montserrat',
+      fontStyle: 'normal',
+      fontWeight: '500',
+      fontSize: '1.25em',
+      lineHeight: '1.5em',
+      textAlign: 'center',
 
-  menus1 = { '1': ['Patient Dashboard', '/doc-patient-dashboard', 1],'2': ['Medical Data', '/doc-medical-data', 0],
-  '3': ['Medications', '/doc-medications', 0],'4': ['Past Records', '/doc-past-records', 0] };
+    }
+  };
+
+  menus1 = { '1': ['Patient Dashboard', '/doc-patient-dashboard', 0],'2': ['Medical Data', '/doc-medical-data', 0],
+  '3': ['Medications', '/doc-medications', 0],'4': ['Past Records', '/doc-past-records', 1] };
   
   menus = { '1': ['Dashboard', '/doctor-dashboard', 1],'2':['Doctor Profile','/doctor-profile']}
+
+  textBtnConfig = {
+    type: "submit",
+    styles: {
+      background: '#fff',
+      color: '#1CB5BD',
+      height: '2em',
+      width: '8em',
+      fontFamily: 'Montserrat',
+      fontStyle: 'normal',
+      fontWeight: '500',
+      fontSize: '1.25em',
+      lineHeight: '1.5em',
+      textAlign: 'center',
+
+    }
+  };
+
+  heightConfig = {
+    type: 'number',
+    label: 'Height',
+    placeholder: '',
+    styling: {
+      height: '2.375em',
+      width: '100%'
+    },
+    validations: {
+      required: '',
+      minLength: '1',
+      maxLength: '2',
+      pattern: ''
+    },
+    patternErrorMessage: ''
+  };
+
+  constructor(private medicationService: MedicationService) { }
+
+  ngOnInit(): void {
+    this.medicationService.getAllPatientRecords(this.patientId)
+      .subscribe(response => {
+        this.allMedicalDataOfAPatient = response
+        console.log(this.allMedicalDataOfAPatient);
+        
+      }
+    )
+  }
+
+  removeOrDeleteMedication(id?: number){
+    this.medicationService.removeOrDeleteMedication(id)
+      .subscribe(
+        response => {
+          this.medicationService.getAllPatientRecords(this.patientId)
+            .subscribe(data => this.allMedicalDataOfAPatient = data)
+        }
+      )
+  }
+
+  
 
 }
