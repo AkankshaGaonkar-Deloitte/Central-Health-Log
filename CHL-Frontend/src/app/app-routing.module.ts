ac32cb540bf280b62537f0b3bec559785418509b
akanksha5 <akanksha5@deloitte.com>
2022-12-06 15:22:52 +0530

    updated code

--- CHL-Frontend/src/app/app-routing.module.ts
+++ CHL-Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DocPatientDashboardComponent } from './components/doctor/doc-patient-dashboard/doc-patient-dashboard.component';
 
 const routes: Routes = [
+  {
+    path : 'doc-patient-dashboard',
+    component : DocPatientDashboardComponent
+  }
+
 ];
 
 @NgModule({
--- CHL-Frontend/src/app/app.module.ts
+++ CHL-Frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputComponent } from './common/input/input.component';
+import { ButtonComponent } from './common/button/button.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
 import { SidebarComponent } from './common/sidebar/sidebar.component';
 import { DropdownComponent } from './common/dropdown/dropdown.component';
 import { CheckboxComponent } from './common/checkbox/checkbox.component';
-import { ButtonComponent } from './common/button/button.component';
+import { DocPatientDashboardComponent } from './components/doctor/doc-patient-dashboard/doc-patient-dashboard.component';
+import { BmiLineGraphComponent } from './components/graphs/bmi-line-graph/bmi-line-graph.component';
+import { PulseLineGraphComponent } from './components/graphs/pulse-line-graph/pulse-line-graph.component';
+import { BpBarGraphComponent } from './components/graphs/bp-bar-graph/bp-bar-graph.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TabNavigationComponent } from './common/tab-navigation/tab-navigation.component';
 
@@ -20,6 +24,10 @@ import { TabNavigationComponent } from './common/tab-navigation/tab-navigation.c
     SidebarComponent,
     DropdownComponent,
     CheckboxComponent,
+    DocPatientDashboardComponent,
+    BmiLineGraphComponent,
+    PulseLineGraphComponent,
+    BpBarGraphComponent,
     ButtonComponent,
     TabNavigationComponent
   ],
