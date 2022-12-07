f8bfd150a6ff17160cfca03849f1607c855513e3
tashmeetkaur-deloitte <tkaurkhanuja@deloitte.com>
2022-12-07 16:01:42 +0530

    added doctor controller

--- CHL-Backend/src/main/java/com/CentralHealthLog/controller/DocRegController.java
+++ CHL-Backend/src/main/java/com/CentralHealthLog/controller/DocRegController.java
@@ -1,27 +1,47 @@
 package com.CentralHealthLog.controller;
 
 import com.CentralHealthLog.entity.Doctor;
+import com.CentralHealthLog.entity.Patient;
 import com.CentralHealthLog.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
+@RequestMapping("/doctor")
 public class DocRegController {
 
     @Autowired
     private DoctorService doctorService;
 
-    @PostMapping("/doctor")
+    @PostMapping
     public ResponseEntity<Doctor> addNewDoctor(@RequestBody Doctor doctor){
-        System.out.println(doctor);
         Doctor savedDoctor = doctorService.saveDoctor(doctor);
         return new ResponseEntity<Doctor>(savedDoctor, HttpStatus.OK);
     }
+    @GetMapping("/user/{username}")
+    public ResponseEntity<Doctor> findPatientByUsername(@PathVariable String username)
+    {
+        Doctor doctor = doctorService.getDoctorByUsername(username);
+        return new ResponseEntity<Doctor>(doctor, HttpStatus.OK);
+    }
+    @GetMapping("phone/{phoneNo}")
+    public ResponseEntity<Doctor> findPatientById(@PathVariable String phoneNo)
+    {
+        Doctor doctor = doctorService.getDoctorByContact(phoneNo);
+        return new ResponseEntity<Doctor>(doctor, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Doctor> findPatientById(@PathVariable long id)
+    {
+        Doctor doctor = doctorService.getDoctorById(id);
+        return new ResponseEntity<Doctor>(doctor, HttpStatus.OK);
+    }
+
 
 }
--- CHL-Backend/src/main/java/com/CentralHealthLog/repository/DoctorRepository.java
+++ CHL-Backend/src/main/java/com/CentralHealthLog/repository/DoctorRepository.java
@@ -1,9 +1,14 @@
 package com.CentralHealthLog.repository;
 
 import com.CentralHealthLog.entity.Doctor;
+import com.CentralHealthLog.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    Optional<Doctor> findByUsername(String username);
+    Optional<Doctor> findByPhoneNo(String phoneNo);
 }
